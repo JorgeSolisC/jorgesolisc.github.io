ed866053afddbd37387bd0fea13d4e944cab0c2e
Jorge Solis <jorge.solis@cceo.com.mx>
2023-09-19 20:01:55 -0600

    Initial commit :tada: Install Vue 3 + Vite

---
+++ .eslintrc.cjs
@@ -0,0 +1,63 @@
+module.exports = {
+	ignorePatterns: [],
+	env: {
+		node: true,
+	},
+	globals: {
+		defineProps: 'readonly',
+		defineEmits: 'readonly',
+		defineExpose: 'readonly',
+	},
+	extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'plugin:vue/vue3-essential'],
+	rules: {
+		'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+		indent: 'off',
+		'space-infix-ops': 'error',
+		'space-before-blocks': "error",
+		'vue/html-indent': [
+			'error',
+			'tab',
+			{
+				attribute: 1,
+				baseIndent: 1,
+				closeBracket: 0,
+			},
+		],
+		'vue/html-closing-bracket-newline': [
+			'error',
+			{
+				singleline: 'never',
+				multiline: 'never',
+			},
+		],
+		'vue/html-self-closing': [
+			'error',
+			{
+				html: {
+					void: 'always',
+					normal: 'always',
+					component: 'always',
+				},
+				svg: 'always',
+				math: 'always',
+			},
+		],
+		'vue/max-attributes-per-line': [
+			'error',
+			{
+				singleline: {
+					max: 10,
+				},
+				multiline: {
+					max: 10,
+				},
+			},
+		],
+		'vue/singleline-html-element-content-newline': 'off',
+		'vue/attribute-hyphenation': 'off',
+		'vue/no-unused-vars': 'error',
+		'no-debugger': 'off',
+		'vue/multi-word-component-names': 'off',
+		'vue/no-reserved-component-names': 'off'
+	},
+};
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import eslintPlugin from "vite-plugin-eslint";
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [vue(), eslintPlugin()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	}
+})
